76725abeb8312f0ef16f5b76fad185c285e95fd3
jetgenobia <guinocorgadjet17@gmail.com>
2025-09-30 04:06:26 +0800

    view time

---
+++ Helpers/TimeHelpers.cs
@@ -0,0 +1,11 @@
+﻿namespace Freelancing.Helpers
+{
+    public static class TimeHelpers
+    {
+        public static DateTime ToPhilippineTime(DateTime utcDateTime)
+        {
+            var phTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Asia/Manila");
+            return TimeZoneInfo.ConvertTimeFromUtc(DateTime.SpecifyKind(utcDateTime, DateTimeKind.Utc), phTimeZone);
+        }
+    }
+}
