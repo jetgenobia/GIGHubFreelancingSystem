bf5cb68ff09d97136403493625c351f6252206bd
jetgenobia <guinocorgadjet17@gmail.com>
2025-11-11 23:34:20 +0800

    prev

--- Models/CreateTerminationViewModel.cs
+++ Models/CreateTerminationViewModel.cs
@@ -19,7 +19,6 @@ namespace Freelancing.Models
         public string TerminationReason { get; set; } = string.Empty;
         
         [Required(ErrorMessage = "Please provide detailed explanation for termination")]
-        [StringLength(2000, ErrorMessage = "Details must be between 50 and 2000 characters")]
         public string TerminationDetails { get; set; } = string.Empty;
         
         [Required(ErrorMessage = "Please specify the final payment amount")]
